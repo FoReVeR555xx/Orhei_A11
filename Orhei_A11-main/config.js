1c9172ef6ede0f07f384a1c1f3911363536bacb5
FoReVeR555xx <rdrubix@gmail.com>
2026-09-26 11:22:39 +0300

    Delete Orhei_A11-main directory

--- Orhei_A11-main/config.js
+++
@@ -1,5 +0,0 @@
-// Public browser configuration. This key is safe to expose only when RLS is enabled.
-window.BOMBA_SUPABASE_CONFIG = {
-  url: 'https://ifcawjyumzzslhtcoeeh.supabase.co',
-  publishableKey: 'sb_publishable_wEgQTvc9XRA6-W392BlKMg_vtyP5Z09'
-};
